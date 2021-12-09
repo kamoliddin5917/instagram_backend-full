33b89d4bf04cb8f396d89b78959f90c0aba00dbb
toxir-brat <toxirli94@gmail.com>
2021-12-09 12:08:00 +0500

    coes

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const { PORT } = require("./config/server");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "https://thismyinstagram.herokuapp.com/" }));
 app.use(express.json());
 app.use("/media", express.static(path.join(__dirname, "./uploads")));
 // app.use(require("./middlewares/auth").AUTH);
